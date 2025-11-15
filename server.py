20ac0ccbd0d2c62b386f81727c1bcf6ced815e4e
zarusoba1116 <91121948+zarusoba1116@users.noreply.github.com>
2025-11-15 14:16:17 +0900

    Update server.py

--- server.py
+++ server.py
@@ -1,17 +1,20 @@
 from threading import Thread
-
 from fastapi import FastAPI
 import uvicorn
 
 app = FastAPI()
 
 @app.get("/")
-async def root():
-	return {"message": "Server is Online."}
+async def root_get():
+    return {"message": "Server is Online."}
+
+@app.head("/")
+async def root_head():
+    return {"message": "Server is Online."}
 
 def start():
-	uvicorn.run(app, host="0.0.0.0", port=8080)
+    uvicorn.run(app, host="0.0.0.0", port=8080)
 
 def server_thread():
-	t = Thread(target=start)
-	t.start()
+    t = Thread(target=start)
+    t.start()
